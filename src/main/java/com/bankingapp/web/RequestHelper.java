bd88aa1c816b71c4b3cfd2f4ae29f88164b03148
joannacm <joannamagdaraog@gmail.com>
2020-09-28 15:32:52 -0400

    test db conn

--- src/main/java/com/bankingapp/web/RequestHelper.java
+++ src/main/java/com/bankingapp/web/RequestHelper.java
@@ -21,8 +21,8 @@ public class RequestHelper {
 	public static Object processPost(HttpServletRequest request, HttpServletResponse response) 
 			throws IOException, ServletException{
 		System.out.println("helper");
-		//boolean bool = new CustomerService().validateEmail("bogum@mail.com");
-		//System.out.println("email available " + bool);
+		boolean bool = new CustomerService().validateEmail("bogum@mail.com");
+		System.out.println("email available " + bool);
 		response.sendRedirect("../index.html");
 		return "testing post";
 	}
