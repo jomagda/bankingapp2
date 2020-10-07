d5410538234aefa9698997f13e28c4ad39c060d9
joannacm <joannamagdaraog@gmail.com>
2020-10-07 14:08:57 -0400

    r

--- src/main/java/com/bankingapp/util/DatabaseConnection.java
+++ src/main/java/com/bankingapp/util/DatabaseConnection.java
@@ -5,15 +5,9 @@ import java.sql.Connection;
 public abstract class DatabaseConnection {
 
 	private String driver = "org.postgresql.Driver";
-	
-	private String databaseURL = "jdbc:postgresql://dbinstance.cbyo2e6eq9t4.us-east-1.rds.amazonaws.com:5432/portfolio";
-	private String databaseUsername = "postgres";
-	private String databasePassword = "root1234";
-	/*
-	 * private String databaseURL = System.getenv("portfoliodburl"); private String
-	 * databaseUsername = System.getenv("portfoliodatabaseusername"); private String
-	 * databasePassword = System.getenv("portfoliodatabasepassword");
-	 */
+	private String databaseURL = System.getenv("portfoliodburl");
+	private String databaseUsername = System.getenv("portfoliodatabaseusername");
+	private String databasePassword = System.getenv("portfoliodatabasepassword");
 	
 	public static DatabaseConnection getInstance() {
         return new DatabaseConnectionImpl();
